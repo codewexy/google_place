8e9c1b04a57cb6e49c2186cee85610a583a1c8f6
codewexy <92027350+codewexy@users.noreply.github.com>
2021-10-23 10:54:09 +0530

    Update autocomplete.dart

--- lib/src/autocomplete/autocomplete.dart
+++ lib/src/autocomplete/autocomplete.dart
@@ -109,6 +109,7 @@ class Autocomplete {
           : _unencodedPath,
       queryParameters,
     );
+    print('uri>>$uri');
     var response = await NetworkUtility.fetchUrl(uri, headers: headers);
     if (response != null) {
       return AutocompleteResponse.parseAutocompleteResult(response);
